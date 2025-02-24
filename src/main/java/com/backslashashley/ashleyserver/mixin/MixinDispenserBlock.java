4905a2355f8339dd33a90c802a5041c5e87034a8
Ashley <backslashashley@outlook.com>
2025-02-25 05:13:07 +1100

    cleaner ghost update fix

--- src/main/java/com/backslashashley/ashleyserver/mixin/MixinDispenserBlock.java
+++ src/main/java/com/backslashashley/ashleyserver/mixin/MixinDispenserBlock.java
@@ -3,8 +3,6 @@ package com.backslashashley.ashleyserver.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.state.BlockState;
-import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
-import net.minecraft.server.entity.living.player.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,17 +10,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
 @Mixin(DispenserBlock.class)
 public class MixinDispenserBlock {
 	@Inject(method = "neighborChanged", at = @At("TAIL"))
 	public void neighborChanged(BlockState state, World world, BlockPos pos, Block neighborBlock, BlockPos neighborPos, CallbackInfo ci) {
-		List<ServerPlayerEntity> players = world.getServer().getPlayerManager().getAll();
-		if (players != null) {
-			for (ServerPlayerEntity player : players) {
-				player.networkHandler.sendPacket(new BlockUpdateS2CPacket(world, pos));
-			}
-		}
+		world.notifyBlockChanged(pos, state, state, 0);
 	}
 }
